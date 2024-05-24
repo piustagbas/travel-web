e13241b9ee4807f336acaef9752827a10f737af0
piustagbas <ptagbajumi12@gmail.com>
2024-05-24 03:16:00 +0100

    first commit

---
+++ travel-web/src/components/Landing.tsx
@@ -0,0 +1,15 @@
+import { Component } from "react"
+
+
+
+export class Landing extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>Landing</div>
+    )
+  }
+}
+
+export default Landing
